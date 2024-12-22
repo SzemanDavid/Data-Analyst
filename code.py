c1bcc9c9fb16498b8757d38a5419781b9f65da3e
SzemanDavid <148859343+SzemanDavid@users.noreply.github.com>
2024-12-23 00:13:18 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-#saját google drive betöltése
+# Loading my Google Drive
 from google.colab import drive
 drive.mount('/content/drive')
 
@@ -6,13 +6,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-pip install openpyxl
+!pip install openpyxl
 
-file_path = '/content/drive/MyDrive/Datasets/sales_data.csv'
-sales_data = pd.read_csv(file_path)
-print(sales_data.head())
+file_path = '/content/drive/MyDrive/Python/Sales-data/sales_data_sample.csv'
+sales_data_sample = pd.read_csv(file_path, encoding='latin1')
+print(sales_data_sample.head())
 
-# Hiányzó adatok keresése
+# Searching for missing data
 print(sales_data.isnull().sum())
 
 # Duplikált sorok eltávolítása
@@ -26,3 +26,28 @@ plt.figure(figsize=(10, 6))
 sns.histplot(sales_data['Sales'], kde=True)
 plt.title('Sales Distribution')
 plt.show()
+
+
+
+# Gépi tanulás modell választás, tanítás és tesztelés
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Adatok előkészítése
+X = df[['month', 'day_of_week', 'product_category']]  # Kiválasztott jellemzők
+y = df['sales']  # Eladások
+
+# Képzés és tesztelés
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Modell betanítása
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Előrejelzés
+y_pred = model.predict(X_test)
+
+# Modell értékelése
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
